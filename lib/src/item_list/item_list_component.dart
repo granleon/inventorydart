e4bd32f2b26a270dc6c3257d93a576b27b036301
Raion314 <raion314@outlook.com>
2019-03-12 21:51:35 -0700

    add mock data

--- lib/src/item_list/item_list_component.dart
+++ lib/src/item_list/item_list_component.dart
@@ -3,6 +3,8 @@ import 'package:angular_components/angular_components.dart';
 import 'package:angular_components/material_expansionpanel/material_expansionpanel.dart';
 import 'package:angular_components/material_expansionpanel/material_expansionpanel_set.dart';
 
+import '../../src/item_model.dart';
+
 @Component(
   selector: 'app-item-list',
   styleUrls: ['item_list_component.css'],
@@ -20,4 +22,9 @@ import 'package:angular_components/material_expansionpanel/material_expansionpan
   ],
   providers: const <dynamic>[materialProviders],
 )
-class ItemListComponent {}
+class ItemListComponent {
+  List<Item> items = [
+    Item('1', 'ApoA', '444444', 'M909090', 5),
+    Item('2', 'ApoB', '444445', 'M909091', 2)
+  ];
+}
