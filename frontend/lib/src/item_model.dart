67338b6dca08a859a78d251079675e6622274694
granleon <raion314@outlook.com>
2019-03-25 18:32:08 -0700

    replaced model with 2D barcode data

--- frontend/lib/src/item_model.dart
+++ frontend/lib/src/item_model.dart
@@ -1,28 +1,27 @@
 class Item {
   String id;
-  String lotnumber;
-  String partnumber;
-  String chem;
-  String chemabbr;
+  String manufacture;
   String expire;
+  String lotnumber;
 
-  Item(this.id, this.lotnumber, this.partnumber, this.chem, this.chemabbr,
-      this.expire);
+  Item(
+    this.id,
+    this.manufacture,
+    this.expire,
+    this.lotnumber,
+  );
 
   factory Item.fromJson(Map<String, dynamic> item) => Item(
-      item['id'],
-      item['lotnumber'],
-      item['partnumber'],
-      item['chem'],
-      item['chemabbr'],
-      item['expire']);
+        item['id'],
+        item['manufacture'],
+        item['expire'],
+        item['lotnumber'],
+      );
 
   Map toJson() => {
         'id': id,
-        'lotnumber': lotnumber,
-        'partnumber': partnumber,
-        'chem': chem,
-        'chemabbr': chemabbr,
+        'manufacture': manufacture,
         'expire': expire,
+        'lotnumber': lotnumber,
       };
 }
